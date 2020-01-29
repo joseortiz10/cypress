f8e87e214f7fcae66fd7671098f4e904daa1064f
dragonjose <jose.ortiz@apsys.mx>
2020-01-28 21:00:24 -0600

    cypress testing

--- cypress/integration/Login/Login.spec.js
+++ cypress/integration/Login/Login.spec.js
@@ -1,10 +1,25 @@
 
 describe("login", () => {
-    before(() => {
-        cy.visit('http://localhost:3000')
+    beforeEach(() => {
+        cy.fixture('login.json').as('usersCredentials')
+        cy.visit('/')
     })
-    it('login', () => {
-        cy.login('adimbptm', 'sesamo')
+    it('login success', () => {
+        cy.getByDataCy('txt-btn-login').contains('LOGIN').should('be.visible')
+        cy.getByDataCy('txt-btn-login').invoke('text').should('contain', 'LOGIN')
+        cy.getByDataCy('btn-login').click()
+        cy.get('@usersCredentials')
+            .then(({ userName, password }) => {
+                cy.login(userName, password)
+            })
+            cy.window().its('store').invoke('getState').should('have.keys', ['oidc'])
+        })
+
+    it('login invalid credentiales', () => {
+        cy.getByDataCy('btn-login').click()
+        cy.login('adimbptm', 'invalidpassword')
+        cy.contains("Invalid username or password").should('be.visible');
     })
 })
+
 
--- src/App.js
+++ src/App.js
@@ -4,41 +4,58 @@ import userManager from './store/helpers/userManager';
 
 /** Custom components import section */
 /** Material UI import section */
-import {Button} from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
+
+import Logo from "./images/logo.png";
+
 
 function App() {
     const { user } = useSelector(state => state.oidc);
-    
+
     const isLoggedIn = () => (user ? true : false);
     const login = () => userManager.signinRedirect();
     const logOut = () => userManager.signoutRedirect();
-    
-    return (
-      <div className="App">
-        <header className="App-header">
-            Identity
-        </header>
-        {
 
-            isLoggedIn() ? (
-                <div>
-                    <span>{`Welcome ${user.profile.preferred_username}`}</span>
-                    <br/>
-                    <Button color="secondary" onClick={logOut}>
-                        Logout
-                    </Button>
-                    <br/>
-                </div>            
-            ) : (
-                <div>
-                    <Button color="secondary" onClick={login}>
-                        Login
-                    </Button>
-                    <br/>
-                </div>
-            )
-        }
-      </div>
+    return (
+        <div className="App">
+            <header className="App-header">
+                Identity
+        </header>
+            {
+
+                isLoggedIn() ? (
+                    <div>
+                        <span>{`Welcome ${user.profile.preferred_username}`}</span>
+                        <br />
+                        <Button
+                            data-cy="btn-logout"
+                            color="secondary"
+                            onClick={logOut}>
+                            Logout
+                        </Button>
+                        <br />
+                        <img
+                            data-cy="img-login-logo"
+							src={Logo}
+							alt='logo'
+						/>
+                    </div>
+                ) : (
+                        <div>
+                            <Button
+                                data-cy="btn-login"
+                                color="secondary"
+                                onClick={login}
+                            >
+                                <Typography variant="subtitle1" data-cy="txt-btn-login">
+                                    LOGIN
+                                </Typography>
+                            </Button>
+                            <br />
+                        </div>
+                    )
+            }
+        </div>
     );
 }
 
